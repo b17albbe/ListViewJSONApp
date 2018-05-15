c08b9a577539d31ffea61c570df46895814b4e46
b17albbe <b17albbe@student.his.se>
2018-05-15 18:45:32 +0200

    added listview + mountains

--- app/src/main/java/com/example/brom/listviewjsonapp/MainActivity.java
+++ app/src/main/java/com/example/brom/listviewjsonapp/MainActivity.java
@@ -4,6 +4,11 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,6 +16,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 // Create a new class, Mountain, that can hold your JSON data
@@ -26,11 +34,58 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
+    private String[] mountainLocations = {"Alps","Alps","Alaska"};
+    private int[] mountainHeights ={4478,4808,6190};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String kjell = new String("Tomte!");
+
+
+        Log.d("brom-debug",kjell);
+        // The onCreate method is run when the app is created.
+        // Before you can implement this you need to create the layout xml files that
+        // will hold/show your data created here. You need three create things:
+        // * my_listview - the ID to the actual layout element that is our ListView.
+        //                 This my_listview element is created in the activity_main.xml file
+        // * list_item_textview - This is a new layout xml file that holds the layout for an
+        //                        individual item in the ListView we are creating
+        // * my_item_textview - This is the ID to the actual TextView that will contain the text for
+        //                      an individual item in the ListView we are creating.
+        // Here you should enter your code that fills the ListView
+        // 1. Create an array
+        String[] rawData = {"Matterhorn","Mont Blanc","Denali"};
+        // 2. Create a List object with your array from step 1 as in-data
+        List<String> listData = new ArrayList<String>(Arrays.asList(rawData));
+        // 3. Create an ArrayAdapter object that connects
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,
+                R.id.my_item_textview,listData);
+        //    * list_item_textview
+        //    * my_item_textview
+        //    * List object created in step 2
+        // 4. Find the ListView layout element "my_listview" and create an object instance
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+        myListView.setAdapter(adapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), mountainNames[position] + "\n" + mountainLocations[position] + "\n" + mountainHeights[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+        // 5. Connect the ArrayAdapter from step 3 with ListView object created in step 4
+        // adapter.add("Hilding");
+        // 6. Style the ListView items according to Material Design
+
+        //    See: https://material.io/guidelines/components/lists.html#lists-specs
+        //    Look for "singel line specs" for "text only" lists and modify the list_item_textview
+        //    accordingly
     }
+
 
     private class FetchData extends AsyncTask<Void,Void,String>{
         @Override
---
+++ app/src/main/java/com/example/brom/listviewjsonapp/Mountain.java
@@ -0,0 +1,10 @@
+package com.example.brom.listviewjsonapp;
+
+/**
+ * Created by b17albbe on 2018-05-15.
+ */
+
+
+public class Mountain {
+
+}
